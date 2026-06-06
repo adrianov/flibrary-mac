ef54e938c0dd4daeea5b10340be8688b8472b139
Peter M Adrianov <peter.adrianov@gmail.com>
2026-06-06 07:02:05 +0300

    refactor: extract BaseModel data access methods into separate compilation unit

--- src/home/flibrary/logic/model/BaseModel.cpp
+++ src/home/flibrary/logic/model/BaseModel.cpp
@@ -1,17 +1,12 @@
 #include "BaseModel.h"
 
-#include "fnd/IsOneOf.h"
 #include "fnd/ScopedCall.h"
-#include "fnd/algorithm.h"
 
-#include "interface/constants/Enums.h"
 #include "interface/constants/ModelRole.h"
 #include "interface/localization.h"
-#include "interface/logic/ILibRateProvider.h"
 #include "interface/logic/IModelProvider.h"
 
 #include "data/DataItem.h"
-#include "data/Genre.h"
 
 using namespace HomeCompa;
 using namespace Flibrary;
@@ -44,121 +39,6 @@ QVariant BaseModel::headerData(const int section, const Qt::Orientation orientat
 	}
 
 	return {};
-}
-
-QVariant BaseModel::data(const QModelIndex& index, const int role) const
-{
-	assert(index.isValid());
-
-	const auto* item = GetInternalPointer(index);
-	if (item->GetType() == ItemType::Books)
-	{
-		if (role == Role::LibRate)
-			return m_libRateProvider->GetLibRateString(item->GetId().toLongLong(), item->GetRawData(BookItem::Column::LibRate));
-
-		if (item->RemapColumn(index.column()) == BookItem::Column::LibRate)
-		{
-			switch (role)
-			{
-				case Qt::DisplayRole:
-					return m_libRateProvider->GetLibRateString(item->GetId().toLongLong(), item->GetRawData(BookItem::Column::LibRate));
-
-				case Qt::ForegroundRole:
-					return m_libRateProvider->GetForegroundBrush(item->GetId().toLongLong(), item->GetRawData(BookItem::Column::LibRate));
-
-				default:
-					break;
-			}
-		}
-	}
-
-	switch (role)
-	{
-		case Qt::DisplayRole:
-		case Qt::ToolTipRole:
-			return GetValue(*item, item->RemapColumn(index.column()));
-
-		case Qt::CheckStateRole:
-		case Role::CheckState:
-			return item->GetCheckState();
-
-		case Role::CheckableColumn:
-			return m_checkableColumn ? QVariant::fromValue(*m_checkableColumn) : QVariant {};
-
-		case Role::Id:
-			return item->GetId();
-
-		case Role::Type:
-			return QVariant::fromValue(item->GetType());
-
-		case Role::IsTree:
-			return false;
-
-		case Role::Flags:
-			return QVariant::fromValue(item->GetFlags());
-
-		case Role::IsRemoved:
-			return item->IsRemoved();
-
-		case Role::ChildCount:
-			return static_cast<qulonglong>(item->GetChildCount());
-
-		case Role::Remap:
-			return item->RemapColumn(index.column());
-
-#define BOOKS_COLUMN_ITEM(NAME) \
-	case Role::NAME:            \
-		return GetValue(*item, BookItem::Column::NAME);
-			BOOKS_COLUMN_ITEMS_X_MACRO
-#undef BOOKS_COLUMN_ITEM
-
-		default:
-			break;
-	}
-
-	return {};
-}
-
-bool BaseModel::setData(const QModelIndex& index, const QVariant& value, const int role)
-{
-	if (index.isValid())
-	{
-		auto* item = GetInternalPointer(index);
-		switch (role)
-		{
-			case Role::CheckState:
-				return item->SetCheckState(value.value<Qt::CheckState>()), true;
-
-			case Qt::EditRole:
-				return true;
-
-			case Role::Flags:
-				return item->SetFlags(value.value<IDataItem::Flags>()), true;
-
-			default:
-				break;
-		}
-
-		return assert(false && "unexpected role"), false;
-	}
-
-	switch (role)
-	{
-		case Role::CheckableColumn:
-			m_checkableColumn = value.toInt();
-			return true;
-
-		case Role::Check:
-			return Check(value, Qt::Checked);
-
-		case Role::Uncheck:
-			return Check(value, Qt::Unchecked);
-
-		default:
-			break;
-	}
-
-	return assert(false && "unexpected role"), false;
 }
 
 bool BaseModel::removeRows(const int row, const int count, const QModelIndex& parent)
@@ -195,16 +75,4 @@ Qt::ItemFlags BaseModel::flags(const QModelIndex& index) const
 IDataItem* BaseModel::GetInternalPointer(const QModelIndex& index) const
 {
 	return static_cast<IDataItem*>(index.internalPointer());
-}
-
-QVariant BaseModel::GetValue(const IDataItem& item, const int column)
-{
-	if (item.GetType() == ItemType::Books && IsOneOf(column, BookItem::Column::SeqNumber, BookItem::Column::Size))
-	{
-		bool       ok     = false;
-		const auto result = item.GetRawData(column).toLongLong(&ok);
-		return ok ? QVariant { result } : QVariant {};
-	}
-
-	return item.GetRawData(column);
 }
